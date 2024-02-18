3f0f41a7cdfd8ae04a4f65b4a0aaf2797399424a
brysonkatuu22 <brysonkatuu77@gmail.com>
2024-02-18 20:29:43 +0300

    push promotion files and changes

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -1,19 +1,26 @@
+'use client'
 import React from 'react'
+import Link from 'next/link'
+
+import { Category } from '../../../../payload/payload-types'
+import { useFilter } from '../../../_providers/Filter'
 
 import classes from './index.module.scss'
-import Link from 'next/link'
-import { Category } from '../../../../payload/payload-types'
 
 type CategoryCardProps = {
   category: Category
 }
 
-const CategoryCard = ({category}: CategoryCardProps) => {
+const CategoryCard = ({ category }: CategoryCardProps) => {
   const media = category.media as Media
-  
+  const { setCategoryFilters } = useFilter()
+
   return (
-    <Link href="/products" className={classes.card} 
-    style={{ backgroundImage: `url(${media.url})`}}
+    <Link
+      href="/products"
+      className={classes.card}
+      style={{ backgroundImage: `url(${media.url})` }}
+      onClick={() => setCategoryFilters([category.id])}
     >
       <p className={classes.title}>{category.title}</p>
     </Link>
--- src/app/_components/Categories/index.tsx
+++ src/app/_components/Categories/index.tsx
@@ -1,25 +1,24 @@
 import React from 'react'
 import Link from 'next/link'
 
-import classes from './index.modules.scss'
 import { Category } from '../../../payload/payload-types'
 import CategoryCard from './CategoryCard'
 
-const Categories = ({ categories}: { categories: Category[] }) => {
+import classes from './index.module.scss'
+
+const Categories = ({ categories }: { categories: Category[] }) => {
   return (
     <section className={classes.container}>
-        <div className={classes.titleWrapper}>
-            <h3>Shop by Categories</h3>
-            <Link href="/products">Show All</Link>
-        </div>
+      <div className={classes.titleWrapper}>
+        <h3>Shop by Categories</h3>
+        <Link href="/products">Show All</Link>
+      </div>
 
-        <div className={classes.list}>
-            {categories.map((category) => (
-                <CategoryCard key={category.id}
-                category={category} />
-            ))}
-
-        </div>
+      <div className={classes.list}>
+        {categories.map(category => {
+          return <CategoryCard key={category.id} category={category} />
+        })}
+      </div>
     </section>
   )
 }
