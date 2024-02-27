4881f9ef8142111e934147adc2e34486a8df8309
eddykimani <edykmoney@gmail.com>
2024-02-28 00:42:16 +0300

    extra changes

--- src/app/_api/fetchDoc.ts
+++ src/app/_api/fetchDoc.ts
@@ -6,6 +6,7 @@ import { PAGE } from '../_graphql/pages'
 import { PRODUCT } from '../_graphql/products'
 import { GRAPHQL_API_URL } from './shared'
 import { payloadToken } from './token'
+import { CATEGORIES } from '../_graphql/categories'
 
 const queryMap = {
   pages: {
@@ -20,6 +21,10 @@ const queryMap = {
     query: ORDER,
     key: 'Orders',
   },
+  categories: {
+    query: CATEGORIES,
+    key: 'categories',
+  }
 }
 
 export const fetchDoc = async <T>(args: {
--- src/payload/seed/home-static.ts
+++ src/payload/seed/home-static.ts
@@ -75,7 +75,6 @@ export const staticHome: Page = {
       },
     ],
     media: '',
-    customHero: undefined
   },
   layout: [
     {
