2bacf18b56b72052d83ba28d02be3ae65ab0d47c
eddykimani <edykmoney@gmail.com>
2024-02-27 23:36:57 +0300

    footer fiel fix

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -6,15 +6,13 @@ import { fetchFooter, fetchGlobals } from '../../_api/fetchGlobals'
 import FooterComponent from './FooterComponent'
 
 
-
-
 export async function Footer() {
   let footer: Footer | null = null
 
   try {
     footer = await fetchFooter()
   } catch (error) {
-  console.log(error);
+  console.log(error)
   }
 
   const navItems = footer?.navItems || []
