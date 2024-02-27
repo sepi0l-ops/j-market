99c7695c64d161c0e50791e7cb8209aa4cc1843d
brysonkatuu22 <brysonkatuu77@gmail.com>
2024-02-28 00:11:43 +0300

    push changes on promotion message

--- src/app/_components/Promotion/index.tsx
+++ src/app/_components/Promotion/index.tsx
@@ -42,9 +42,9 @@ const Promotion = () => {
       <div className={classes.textBox}>
         <h3 className={classes.title}>Deals of the Month</h3>
         <p>
-          Get ready for a shopping experience like never before with our Deals of the Month! Every
+          Get ready for a shopping experience like never before!!!Every
           purchase comes with exclusive perks and offers, making this month a celebration of savvy
-          choices and amazing deals. Don't miss out! 🎁🛒
+          choices and amazing deals. Don't miss out!🎁🛒
         </p>
 
         <ul className={classes.stats}>
